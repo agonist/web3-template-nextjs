d81582b56588c38289509c4c5070217fca03775a
bas <bastien.billey@gmail.com>
2023-01-27 15:13:16 +0800

    add next-seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -42,6 +42,11 @@ export default function RootLayout({
 
   return (
     <html lang="en" className="dark">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width" />
+        <meta name="robots" content="index,follow" />
+      </head>
       <body>
         <WagmiConfig client={wagmiClient}>
           <RainbowKitProvider chains={chains} theme={darkTheme()}>
